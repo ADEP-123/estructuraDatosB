8f5812b3da83c0501a05cfbc1d029361e494da4a
Elpanchez <santiagosanchez.ribero@gmail.com>
2024-05-19 13:30:53 -0500

    feat: :sparkles: Agregando nuevos colores para los discos

--- js/modules/game/scripts/randomColors.js
+++ js/modules/game/scripts/randomColors.js
@@ -3,7 +3,7 @@ import Variables from "../../../variables.js"
 export default function randomColors() {
     const variables = new Variables;
 
-    const colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51', '#3a86ff']
+    const colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51', '#3a86ff', '#523961', '#f22b56', '#a22365', '#9a8fc8', '#ffc0cb', '#2f58a5']
 
     let randNum = Math.trunc(Math.random() * colors.length);
 
